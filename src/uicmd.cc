eb4d7d2271794959a382a20ef788b88bc4589cec
jcid <devnull@localhost>
2008-10-04 20:51:26 +0200

    - Workaround for TabGroup max 128 limit.

--- src/uicmd.cc
+++ src/uicmd.cc
@@ -228,6 +228,11 @@ BrowserWindow *UIcmd_tab_new(const void *vbw)
    BrowserWindow *old_bw = (BrowserWindow*)vbw;
    UI *ui = BW2UI(old_bw);
 
+   // WORKAROUND: limit the number of tabs because of a fltk bug
+   if (ui->tabs()->children() >= 127)
+      return a_UIcmd_browser_window_new(ui->window()->w(), ui->window()->h(),
+                                        vbw);
+
    // Create and set the UI
    UI *new_ui = new UI(0, 0, ui->w(), ui->h(), "Label", ui);
    new_ui->tabs(ui->tabs());
