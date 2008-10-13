25ee636dbb5de48d2528ad747c99b2171054385d
jcid <devnull@localhost>
2008-10-13 14:29:51 +0200

    .

--- src/IO/about.c
+++ src/IO/about.c
@@ -240,7 +240,7 @@ const char *AboutSplash=
 "<tr>\n"
 " <td bgcolor='#CCCCCC'>\n"
 "  <h4>Release overview</h4>\n"
-"  October ??, 2008\n"
+"  October 14, 2008\n"
 "<tr>\n"
 " <td bgcolor='#FFFFFF'>\n"
 "  <table border='0' cellspacing='0' cellpadding='5'>\n"
