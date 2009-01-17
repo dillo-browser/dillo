9408a6f6bcc4d65c96d4dbfbafa49325778e1fb9
Jeremy Henty <onepoint@starurchin.org>
2009-01-17 10:59:15 +0000

    [mq]: add-dpid-copyright

--- dpid/dpid_common.c
+++ dpid/dpid_common.c
@@ -1,3 +1,14 @@
+/*
+ * File: dpid_common.c
+ *
+ * Copyright 2008 Jorge Arellano Cid <jcid@dillo.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
