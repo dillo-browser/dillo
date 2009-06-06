65529fbec6e7c8e3d1baab28cbfbce23de4635ba
Johannes Hofmann <Johannes.Hofmann@gmx.de>
2009-06-06 21:23:04 +0200

    add Copyright

--- src/xembed.cc
+++ src/xembed.cc
@@ -1,3 +1,14 @@
+/*
+ * File: xembed.cc
+ *
+ * Copyright (C) 2009 Jorge Arellano Cid <jcid@dillo.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
