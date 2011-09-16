3ddb676d547c90907e5d4631a63dcd1030e6f4e2
corvid <corvid@lavabit.com>
2011-09-16 05:10:29 +0000

    update TLDs
    What an awful situation it is in general.

--- dpi/cookies.c
+++ dpi/cookies.c
@@ -1037,11 +1037,11 @@ static uint_t Cookies_internal_dots_required(const char *host)
 
       if (tld_len > 0) {
          /* These TLDs were chosen by examining the current publicsuffix list
-          * in January 2010 and picking out those where it was simplest for
+          * in September 2011 and picking out those where it was simplest for
           * them to describe the situation by beginning with a "*.[tld]" rule.
           */
-         const char *const tlds[] = {"ar","au","bd","bn","bt","ck","cy","do",
-                                     "eg","er","et","fj","fk","gt","gu","id",
+         const char *const tlds[] = {"ar","au","bd","bn","bt","ck","cy",
+                                     "er","et","fj","fk","gt","gu","id",
                                      "il","jm","ke","kh","kw","ml","mm","mt",
                                      "mz","ni","np","nz","om","pg","py","qa",
                                      "sv","tr","uk","uy","ve","ye","yu","za",
