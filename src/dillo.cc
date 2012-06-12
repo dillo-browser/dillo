c9a0705fcc1759c6a9d75f62d24a759f07e84d96
Johannes Hofmann <Johannes.Hofmann@gmx.de>
2012-06-12 22:22:20 +0200

    simplify startup without URLs on the command line
    Submitted by: 123 <p37sitdu@lavabit.com>

--- src/dillo.cc
+++ src/dillo.cc
@@ -410,11 +410,7 @@ int main(int argc, char **argv)
 
    if (idx == argc) {
       /* No URLs/files on cmdline. Send startup screen */
-      if (dStrAsciiCasecmp(URL_SCHEME(prefs.start_page), "about") == 0 &&
-          strcmp(URL_PATH(prefs.start_page), "blank") == 0)
-         a_UIcmd_open_url(bw, NULL);
-      else
-         a_UIcmd_open_url(bw, prefs.start_page);
+      a_UIcmd_open_url(bw, prefs.start_page);
    } else {
       for (int i = idx; i < argc; i++) {
          DilloUrl *start_url = makeStartUrl(argv[i], local);
