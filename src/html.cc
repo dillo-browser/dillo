f50e3982ae85b6a3072fb3c97cfdc88bb2dc9fb0
corvid <corvid@lavabit.com>
2012-11-10 17:55:34 +0000

    rm MSG blank lines

--- src/html.cc
+++ src/html.cc
@@ -2967,10 +2967,10 @@ void a_Html_load_stylesheet(DilloHtml *html, DilloUrl *url)
          ++html->bw->NumPendingStyleSheets;
          a_Bw_add_client(html->bw, ClientKey, 0);
          a_Bw_add_url(html->bw, url);
-         MSG("NumPendingStyleSheets=%d", html->bw->NumPendingStyleSheets);
+         MSG("NumPendingStyleSheets=%d\n", html->bw->NumPendingStyleSheets);
       }
    }
-   MSG("\n");
+   _MSG("\n");
 }
 
 /*
