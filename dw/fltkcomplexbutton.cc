0c71f4f7962733a59d7113979c5a7afe6dc1d176
corvid <corvid@lavabit.com>
2012-12-10 21:34:23 +0000

    fltkcomplexbutton don't need widget tracker
    It seems that the idea of a widget tracker is so that you can do a callback
    and, after you return, check whether the widget is still there before using
    it. We're just returning from the handle(), though.

--- dw/fltkcomplexbutton.cc
+++ dw/fltkcomplexbutton.cc
@@ -98,8 +98,6 @@ int ComplexButton::handle(int event) {
         !(Fl::event_state() & (FL_SHIFT | FL_CTRL | FL_ALT | FL_META))) {
       value(1);
       set_changed();
-      Fl_Widget_Tracker wp(this);
-      if (wp.deleted()) return 1;
       if (when() & FL_WHEN_RELEASE) do_callback();
       return 1;
     } else return 0;
