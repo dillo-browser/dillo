35b754a7c3baa28b29885395ab0a3bbc188fb236
corvid <corvid@lavabit.com>
2013-01-08 22:56:43 +0000

    fix an at-least-conceivable fd leak with cookiesrc on error
    You can trigger ferror() by putting in a fputc() or the like, but I'm not
    immediately sure of any good way to trigger it without changing the code...

--- src/cookies.c
+++ src/cookies.c
@@ -260,6 +260,7 @@ static int Cookie_control_init(void)
       if (!rc && ferror(stream)) {
          MSG("Cookies1: Error while reading rule from cookiesrc: %s\n",
              dStrerror(errno));
+         fclose(stream);
          return 2; /* bail out */
       }
 
