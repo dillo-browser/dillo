ff6ac1a7aed80bb4646e0e33114542ff0c179c23
Sebastian Geerken <devnull@localhost>
2013-01-22 20:01:18 +0100

    Fixed last commit.

--- dw/textblock.cc
+++ dw/textblock.cc
@@ -1795,7 +1795,7 @@ void Textblock::addSpace (core::style::Style *style)
  * Add a break option (see setBreakOption() for details). Used instead
  * of addStyle for ideographic characters.
  */
-inline void Textblock::addBreakOption (core::style::Style *style)
+void Textblock::addBreakOption (core::style::Style *style)
 {
    int wordIndex = words->size () - 1;
    if (wordIndex >= 0)
