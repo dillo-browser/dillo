0128efee08d42bd4efa1fda3bc12664b216712ae
Sebastian Geerken <devnull@localhost>
2013-03-04 23:45:56 +0100

    Simplified containers test.

--- test/containers.cc
+++ test/containers.cc
@@ -6,42 +6,34 @@ using namespace lout::container::typed;
 
 void testHashSet ()
 {
+   puts ("--- testHashSet ---");
+
    HashSet<String> h(true);
 
    h.put (new String ("one"));
    h.put (new String ("two"));
    h.put (new String ("three"));
    
-   Iterator<String> it = h.iterator ();
-   while (it.hasNext ()) {
-      String *o = it.getNext ();
-      printf ("%s\n", o->chars());
-   }
+   puts (h.toString());
 }
 
 void testHashTable ()
 {
+   puts ("--- testHashTable ---");
+
    HashTable<String, Integer> h(true, true);
 
    h.put (new String ("one"), new Integer (1));
    h.put (new String ("two"), new Integer (2));
    h.put (new String ("three"), new Integer (3));
    
-   for (Iterator<String> it = h.iterator (); it.hasNext (); ) {
-      String *k = it.getNext ();
-      Integer *v = h.get (k);
-      printf ("%s -> %d\n", k->chars(), v->getValue());
-   }
+   puts (h.toString());
 
    h.put (new String ("one"), new Integer (4));
    h.put (new String ("two"), new Integer (5));
    h.put (new String ("three"), new Integer (6));
-   
-   for (Iterator<String> it = h.iterator (); it.hasNext (); ) {
-      String *k = it.getNext ();
-      Integer *v = h.get (k);
-      printf ("%s -> %d\n", k->chars(), v->getValue());
-   }
+
+   puts (h.toString());
 }
 
 int main (int argc, char *argv[])
