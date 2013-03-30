c7bdc7b795039aa563dc8d7bfa2b4c0e7625a049
Sebastian Geerken <devnull@localhost>
2013-03-30 15:24:38 +0100

    Added/updated copyright notices.

--- dw/textblock_linebreaking.cc
+++ dw/textblock_linebreaking.cc
@@ -1,3 +1,25 @@
+/*
+ * Dillo Widget
+ *
+ * Copyright 2005-2007, 2012-2013 Sebastian Geerken <sgeerken@dillo.org>
+ *
+ * (Parts of this file were originally part of textblock.cc.)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "textblock.hh"
 #include "hyphenator.hh"
 #include "../lout/msg.h"
