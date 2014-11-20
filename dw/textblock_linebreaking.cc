e852decfc41eacfb903dfe1af20fe4bdaba67658
Sebastian Geerken <devnull@localhost>
2014-11-20 01:01:25 +0100

    Textblock::removeTemporaryLines: call accumulateWordData for affected words.

--- dw/textblock_linebreaking.cc
+++ dw/textblock_linebreaking.cc
@@ -2096,8 +2096,23 @@ void Textblock::showMissingLines ()
 
 void Textblock::removeTemporaryLines ()
 {
-   lines->setSize (nonTemporaryLines);
-   DBG_OBJ_SET_NUM ("lines.size", lines->size ());
+   DBG_OBJ_ENTER0 ("construct.line", 0, "removeTemporaryLines");
+
+   if (nonTemporaryLines < lines->size ()) {
+      lines->setSize (nonTemporaryLines);
+      DBG_OBJ_SET_NUM ("lines.size", lines->size ());
+
+      // For words which will be added, the values calculated before in
+      // accumulateWordData() are wrong, so it is called again. (Actually, the
+      // words from the first temporary line are correct, but for simplicity,
+      // we re-calculate all.)
+      int firstWord =
+         lines->size () > 0 ? lines->getLastRef()->lastWord + 1 : 0;
+      for (int i = firstWord; i < words->size (); i++)
+         accumulateWordData (i);
+   }
+
+   DBG_OBJ_LEAVE ();
 }
 
 int Textblock::getSpaceShrinkability(struct Word *word)
