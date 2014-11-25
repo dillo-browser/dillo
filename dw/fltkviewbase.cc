20ce82ab74c1732359c9fd8ae91e8d4805a20150
Jorge Arellano Cid <jcid@dillo.org>
2014-11-25 19:09:54 +0000

    Fix a problem with FLTK's fl_oldfocus variable
    http://lists.dillo.org/pipermail/dillo-dev/2014-November/010299.html
    
    and, from commit text for changeset eb902ac9fc66
    "Starting with fltk-1.3.3, we can't use fl_oldfocus, this patch
    fixes this problem.
    
    IOW. this patch is necessary to link with fltk-1.3.3."

--- dw/fltkviewbase.cc
+++ dw/fltkviewbase.cc
@@ -27,8 +27,6 @@
 #include <stdio.h>
 #include "../lout/msg.h"
 
-extern Fl_Widget* fl_oldfocus;
-
 using namespace lout::object;
 using namespace lout::container::typed;
 
@@ -364,7 +362,9 @@ int FltkViewBase::handle (int event)
       }
       return 1;
    case FL_UNFOCUS:
-      focused_child = fl_oldfocus;
+      // FLTK delivers UNFOCUS to the previously focused widget
+      if (find(Fl::focus()) < children())
+         focused_child = Fl::focus(); // remember the focused child!
       return 0;
    case FL_KEYBOARD:
       if (Fl::event_key() == FL_Tab)
