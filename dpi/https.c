e14906ab1407f800e3a06d57c838a2887cf65a2f
Johannes Hofmann <Johannes.Hofmann@gmx.de>
2018-04-06 09:34:31 +0200

    Support OpenSSL 1.1.0
    taken-from: pkgsrc (Ryo ONODERA)
    submitted-by: Jun Ebihara <jun@soum.co.jp>

--- dpi/https.c
+++ dpi/https.c
@@ -476,7 +476,11 @@ static int handle_certificate_problem(SSL * ssl_connection)
       case X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT:
          /*Either self signed and untrusted*/
          /*Extract CN from certificate name information*/
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
          if ((cn = strstr(remote_cert->name, "/CN=")) == NULL) {
+#else
+         if ((cn = strstr(X509_get_subject_name(remote_cert), "/CN=")) == NULL) {
+#endif
             strcpy(buf, "(no CN given)");
          } else {
             char *cn_end;
