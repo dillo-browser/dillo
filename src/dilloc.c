309beacd8916014229118370285d1da07ebea865
Rodrigo Arias Mallo <rodarima@gmail.com>
2025-04-26 10:05:41 +0200

    Use the first control socket if unique

--- src/dilloc.c
+++ src/dilloc.c
@@ -13,23 +13,59 @@
 
 #include "dlib/dlib.h"
 
-#include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/un.h>
 #include <unistd.h>
-#include <errno.h>
 
-int connect_to_socket(int *sock)
+/* Try to locate dillo if there is only one process */
+char *get_unique_dillo_pid(void)
 {
-   char *pid = getenv("DILLO_PID");
-   if (pid == NULL) {
-      fprintf(stderr, "DILLO_PID not set, stopping\n");
-      return -1;
+   char path[1024];
+   if (snprintf(path, 1024, "%s/.dillo/ctl", dGethomedir()) >= 1024) {
+      fprintf(stderr, "path too long\n");
+      return NULL;
    }
 
+   struct dirent *ep;
+   DIR *dp = opendir (path);
+   if (dp == NULL) {
+      fprintf(stderr, "error: cannot open %s directory: %s\n",
+            path, strerror(errno));
+      return NULL;
+   }
+
+   int npids = 0;
+   char *pid = NULL;
+
+   while ((ep = readdir (dp)) != NULL) {
+      const char *n = ep->d_name;
+      /* Skip . and .. directories */
+      if (!strcmp(n, ".") || !strcmp(n, ".."))
+         continue;
+      npids++;
+      if (!pid)
+         pid = dStrdup(n);
+   }
+
+   closedir(dp);
+
+   if (npids == 1)
+      return pid;
+
+   dFree(pid);
+
+   return NULL;
+}
+
+int connect_to_socket(const char *pid, int *sock)
+{
    int fd;
    if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
       fprintf(stderr, "socket() failed: %s\n", strerror(errno));
@@ -49,7 +85,8 @@ int connect_to_socket(int *sock)
    int slen = sizeof(addr);
 
    if (connect(fd, (struct sockaddr *) &addr, slen) != 0) {
-      fprintf(stderr, "cannot connect to control socket: %s\n", strerror(errno));
+      fprintf(stderr, "error: cannot connect to %s: %s\n",
+            addr.sun_path, strerror(errno));
       return -1;
    }
 
@@ -65,8 +102,17 @@ int main(int argc, char *argv[])
       return 1;
    }
 
+   /* First try the env var */
+   char *pid = getenv("DILLO_PID");
+
+   /* If not set, try to search for a single pid socket file */
+   if (!pid && (pid = get_unique_dillo_pid()) == NULL) {
+      fprintf(stderr, "error: cannot find unique control socket, set DILLO_PID\n");
+      return 1;
+   }
+
    int fd;
-   if (connect_to_socket(&fd) != 0)
+   if (connect_to_socket(pid, &fd) != 0)
       return 1;
 
    Dstr *cmd = dStr_new("");
