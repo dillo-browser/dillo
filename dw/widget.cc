53c48679ed97a394be21436e44530e3f8864fce3
Cameron Paul <cpaul37@gmail.com>
2025-06-02 18:11:09 -0500

    Fix max-width not being respected with margin auto

--- dw/widget.cc
+++ dw/widget.cc
@@ -1039,11 +1039,19 @@ void Widget::calcFinalWidth (style::Style *style, int refWidth,
                                forceValue);
    DBG_OBJ_MSGF ("resize", 1, "w = %d, width = %d", w, width);
 
-   if (width.total != -1)
-      w = width.total;
+   int marginLeft = 0;
+   int marginRight = 0;
 
-   int marginLeft = width.marginLeft;
-   int marginRight = width.marginRight;
+   if (width.total != -1) {
+      w = width.total;
+      marginLeft = width.marginLeft;
+      marginRight = width.marginRight;
+   } else {
+      BoxWidth bw = calcWidth (style::createAbsLength(w), refWidth, refWidget,
+                               limitMinWidth, forceValue);
+      marginLeft = bw.marginLeft;
+      marginRight = bw.marginRight;
+   }
 
    /* Only correct w if not set to auto (-1) */
    if (w != -1) {
@@ -1068,7 +1076,7 @@ void Widget::calcFinalWidth (style::Style *style, int refWidth,
          marginRight = minWidth.marginRight;
       }
 
-      if (maxWidth.total != -1 && w > maxWidth.total) {
+      if (maxWidth.total != -1 && w >= maxWidth.total) {
          w = maxWidth.total;
          marginLeft = maxWidth.marginLeft;
          marginRight = maxWidth.marginRight;
