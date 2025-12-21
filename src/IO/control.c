74785c96c2fb6a359c736b57d6e66fa9bf3ce219
Rodrigo Arias Mallo <rodarima@gmail.com>
2025-12-21 20:49:59 +0100

    Implement non-blocking control socket
    When using dilloc in a pipe to perform a filter operation like:
    
      $ dilloc dump | filter | dilloc load
    
    There is a chance that the dump command begins the execution before all
    the data is dumped from dilloc dump. This causes dillo to hang as soon
    as it starts handling the load command, which is blocking, and no
    progress can be done in the dump side.
    
    To prevent deadlocks both in the read or write side, all operations are
    now non-blocking. The UI will continue to be responsive, even if dillo
    is loading the current page.
    
    Fixes: https://bug.dillo-browser.org/515/

--- src/IO/control.c
+++ src/IO/control.c
@@ -39,21 +39,43 @@
 #include "msg.h"
 #include "timeout.hh"
 
+enum control_req_stage {
+   REQ_READ_COMMAND,
+   REQ_READ_BODY,
+   REQ_PREPARE_REPLY,
+   REQ_SWITCH_WRITE,
+   REQ_WRITE_REPLY,
+   REQ_CLOSE,
+};
+
+struct control_req {
+   int fd;
+   enum control_req_stage stage;
+   Dstr *cmd;
+   Dstr *body;
+   Dstr *reply;
+   size_t reply_offset;
+};
+
 /* One control fd per process */
 static int control_fd = -1;
 static char *control_path = NULL;
 
 /* The BrowserWindow that we are currently waiting to finish */
 static BrowserWindow *bw_waiting = NULL;
-static FILE *bw_waiting_file = NULL;
+static struct control_req *bw_waiting_req = NULL;
 
 static void handle_wait_timeout(void *data);
 
-static void
-cmd_load(FILE *f, int fd, int raw)
+/* Read the given non-blocking fd, read available data into output.
+ * Returns:
+ *    0:  when the end of file has been reached.
+ *   +1:  if there is more data to be read.
+ *   <0: other unexpected error.
+ */
+static int
+Control_read_fd(int fd, Dstr *output)
 {
-   _MSG("cmd_load()\n");
-   Dstr *dstr = dStr_sized_new(1);
    ssize_t r;
    do {
       char buf[BUFSIZ];
@@ -62,42 +84,282 @@ cmd_load(FILE *f, int fd, int raw)
       if (r < 0) {
          if (errno == EINTR) {
             continue;
+         } else if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            /* Reached end of available data, try later */
+            return +1;
          } else {
-            MSG_ERR("read failed in load command: %s\n", dStrerror(errno));
-            exit(1);
+            MSG_ERR("Control_read_fd: read failed: %s\n", dStrerror(errno));
+            return -1;
          }
       } else if (r > 0) {
-         dStr_append_l(dstr, buf, r);
+         dStr_append_l(output, buf, r);
       }
    } while (r);
 
-   BrowserWindow *bw = a_UIcmd_get_first_active_bw();
-   /* Load the given content in the current page */
-   DilloUrl *url = a_Url_new(a_UIcmd_get_location_text(bw), NULL);
-   if (url == NULL) {
-      fprintf(f, "0\ncannot get current url");
-      return;
-   }
-
-   if (raw) {
-      a_Cache_entry_inject(url, dstr->str, dstr->len, 0);
-   } else {
-      a_Cache_entry_inject(url, dstr->str, dstr->len,
-            CA_GotHeader | CA_GotLength);
-   }
-
-   a_UIcmd_repush(bw);
-
-   dStr_free(dstr, 1);
-   a_Url_free(url);
-
-   fprintf(f, "0\n");
+   return 0;
 }
 
-static void Control_read_cb(int fd, void *data)
+static int
+Control_write_fd(int fd, Dstr *input, size_t *ptr_offset)
 {
-   _MSG("Control_read_cb called\n");
-   Dstr *dstr = dStr_sized_new(BUFSIZ);
+   size_t offset = *ptr_offset;
+   ssize_t w;
+
+   while ((size_t) input->len > offset) {
+      const char *src = input->str + offset;
+      size_t left = input->len - offset;
+      w = write(fd, src, left);
+      if (w < 0) {
+         if (errno == EINTR) {
+            continue;
+         } else if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            /* Cannot write more data, return error */
+            *ptr_offset = offset;
+            return +1;
+         } else {
+            MSG_ERR("Control_write_fd: write failed: %s\n", dStrerror(errno));
+            return -1;
+         }
+      } else if (w > 0) {
+         offset += w;
+      }
+   }
+
+   /* Save offset just in case */
+   *ptr_offset = offset;
+
+   return 0;
+}
+
+/* Create new request structure */
+static struct control_req *Control_req_new(int fd)
+{
+   struct control_req *req = dMalloc0(sizeof(struct control_req));
+   req->fd = fd;
+   req->cmd = dStr_sized_new(128);
+   req->body = dStr_sized_new(1);
+   req->reply = dStr_sized_new(64);
+   req->stage = REQ_READ_COMMAND;
+
+   return req;
+}
+
+static void Control_req_free(struct control_req *req)
+{
+   dStr_free(req->cmd, 1);
+   dStr_free(req->body, 1);
+   dStr_free(req->reply, 1);
+   dFree(req);
+}
+
+static void Control_req_read_cmd(struct control_req *req)
+{
+   ssize_t r;
+   int err = 0;
+   do {
+      /* Read characters one by one */
+      char buf[1];
+      r = read(req->fd, buf, 1);
+      if (r < 0) {
+         if (errno == EINTR) {
+            continue;
+         } else if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            /* Reached end of available data for now, try later */
+            return;
+         } else {
+            MSG_ERR("Control_req_read_cmd: read failed: %s\n", dStrerror(errno));
+            err = 1;
+            break;
+         }
+      } else if (r == 1) {
+         dStr_append_l(req->cmd, buf, 1);
+      }
+      if (buf[0] == '\n')
+         break;
+   } while (r);
+
+   if (err || req->cmd->len == 0) {
+      /* Problem reading command or empty (may be caused by a reset
+       * connection). Go straight to close the request. */
+      req->stage = REQ_CLOSE;
+   } else {
+      /* Got command, strip new line and line feed */
+      char *cmd = req->cmd->str;
+      for (char *p = cmd; *p != '\0'; p++) {
+         if (*p == '\n' || *p == '\r') {
+            *p = '\0';
+            break;
+         }
+      }
+
+      /* Only determine if we need to read the body or not */
+      if (!strcmp(cmd, "load") || !strcmp(cmd, "rawload"))
+         req->stage = REQ_READ_BODY;
+      else
+         req->stage = REQ_PREPARE_REPLY;
+   }
+}
+
+static void Control_req_prepare_reply(struct control_req *req)
+{
+   int done = 1;
+   const char *cmd = req->cmd->str;
+   Dstr *r = req->reply;
+   BrowserWindow *bw = a_UIcmd_get_first_active_bw();
+
+   if (strcmp(cmd, "help") == 0) {
+      dStr_sprintfa(r, "0\n");
+      dStr_sprintfa(r, "Commands return 0 on success or non-zero on error.\n");
+      dStr_sprintfa(r, "Available commands:\n");
+      dStr_sprintfa(r, " ping          Check if dillo replies correctly:\n");
+      dStr_sprintfa(r, " pid           Print PID of selected dillo process\n");
+      dStr_sprintfa(r, " reload        Reload the current tab\n");
+      dStr_sprintfa(r, " ready         Exits with 0 if finished loading, 1 otherwise\n");
+      dStr_sprintfa(r, " open URL      Open the given URL in the current tab\n");
+      dStr_sprintfa(r, " url           Print the url in the current tab\n");
+      dStr_sprintfa(r, " dump          Print the content of the current tab\n");
+      dStr_sprintfa(r, " hdump         Print the HTTP headers of the current tab\n");
+      dStr_sprintfa(r, " load          Replace the content in the current tab by stdin\n");
+      dStr_sprintfa(r, " rawload       Replace the HTTP headers and content of the current\n");
+      dStr_sprintfa(r, "               tab by stdin\n");
+      dStr_sprintfa(r, " quit          Close dillo\n");
+      dStr_sprintfa(r, " wait [T]      Wait until the current tab has finished loading\n");
+      dStr_sprintfa(r, "               at most T seconds (default 60.0). Wait forever with\n");
+      dStr_sprintfa(r, "               T set to 0.\n");
+   } else if (strcmp(cmd, "ping") == 0) {
+      dStr_sprintfa(r, "0\npong\n");
+   } else if (strcmp(cmd, "pid") == 0) {
+      dStr_sprintfa(r, "0\n%d\n", getpid());
+   } else if (strcmp(cmd, "reload") == 0) {
+      a_UIcmd_reload_all_active();
+      dStr_sprintfa(r, "0\n");
+   } else if (strcmp(cmd, "ready") == 0) {
+      dStr_sprintfa(r, "%d\n", a_UIcmd_has_finished(bw) ? 0 : 1);
+   } else if (strncmp(cmd, "open ", 5) == 0) {
+      a_UIcmd_open_urlstr(bw, cmd+5);
+      dStr_sprintfa(r, "0\n");
+   } else if (strcmp(cmd, "url") == 0) {
+      dStr_sprintfa(r, "0\n%s\n", a_UIcmd_get_location_text(bw));
+   } else if (strcmp(cmd, "dump") == 0) {
+      DilloUrl *url = a_Url_new(a_UIcmd_get_location_text(bw), NULL);
+      char *buf;
+      int size;
+      dStr_sprintfa(r, "0\n");
+      if (a_Capi_get_buf(url, &buf, &size))
+         dStr_append_l(r, buf, size);
+      a_Url_free(url);
+   } else if (strcmp(cmd, "hdump") == 0) {
+      DilloUrl *url = a_Url_new(a_UIcmd_get_location_text(bw), NULL);
+      Dstr *header = a_Cache_get_header(url);
+      if (header == NULL) {
+         dStr_sprintfa(r, "1\nCurrent url not cached");
+      } else {
+         dStr_sprintfa(r, "0\n");
+         dStr_append_l(r, header->str, header->len);
+      }
+      a_Url_free(url);
+   } else if (strcmp(cmd, "load") == 0) {
+      DilloUrl *url = a_Url_new(a_UIcmd_get_location_text(bw), NULL);
+      if (url) {
+         a_Cache_entry_inject(url, req->body->str, req->body->len,
+               CA_GotHeader | CA_GotLength);
+         a_UIcmd_repush(bw);
+         dStr_sprintfa(r, "0\n");
+         a_Url_free(url);
+      } else {
+         dStr_sprintfa(r, "1\ncannot get current url");
+      }
+   } else if (strcmp(cmd, "rawload") == 0) {
+      DilloUrl *url = a_Url_new(a_UIcmd_get_location_text(bw), NULL);
+      if (url) {
+         a_Cache_entry_inject(url, req->body->str, req->body->len, 0);
+         a_UIcmd_repush(bw);
+         dStr_sprintfa(r, "0\n");
+         a_Url_free(url);
+      } else {
+         dStr_sprintfa(r, "1\ncannot get current url");
+      }
+   } else if (strcmp(cmd, "quit") == 0) {
+      a_UIcmd_close_all_bw((void *)1);
+      dStr_sprintfa(r, "0\n");
+   } else if (strncmp(cmd, "cmd ", 4) == 0) {
+      const char *cmdname = cmd + 4;
+      int ret = a_UIcmd_by_name(bw, cmdname);
+      dStr_sprintfa(r, "%d\n", ret == 0 ? 0 : 1);
+   } else if (strcmp(cmd, "wait") == 0 || strncmp(cmd, "wait ", 5) == 0) {
+      float timeout = 60.0f; /* 1 minute by default */
+      /* Contains timeout argument? */
+      if (cmd[4] == ' ')
+         timeout = atof(&cmd[5]);
+      if (a_UIcmd_has_finished(bw)) {
+         dStr_sprintfa(r, "0\n");
+      } else {
+         if (bw_waiting) {
+            dStr_sprintfa(r, "1\nalready waiting\n");
+         } else {
+            bw_waiting = bw;
+            bw_waiting_req = req;
+            done = 0;
+            /* If timeout is 0 or negative, wait forever */
+            if (timeout > 0.0f)
+               a_Timeout_add(timeout, handle_wait_timeout, NULL);
+         }
+      }
+   } else {
+      dStr_sprintfa(r, "1\nunknown command: %s\n", cmd);
+   }
+
+   if (done)
+      req->stage = REQ_SWITCH_WRITE;
+}
+
+/* Process incoming or outgoing data from a control request */
+static void Control_req_read_cb(int fd, void *data)
+{
+   struct control_req *req = data;
+
+   /* Note: These are not if-elses on purpose. */
+
+   if (req->stage == REQ_READ_COMMAND)
+      Control_req_read_cmd(req);
+
+   if (req->stage == REQ_READ_BODY) {
+      /* Read until the end of the data or error */
+      int ret = Control_read_fd(fd, req->body);
+      if (ret == 0)
+         req->stage = REQ_PREPARE_REPLY;
+      else if (ret < 0)
+         req->stage = REQ_CLOSE;
+   }
+
+   if (req->stage == REQ_PREPARE_REPLY)
+      Control_req_prepare_reply(req);
+
+   if (req->stage == REQ_SWITCH_WRITE) {
+      /* Switch callback to write */
+      a_IOwatch_remove_fd(fd, DIO_READ);
+      a_IOwatch_add_fd(fd, DIO_WRITE, Control_req_read_cb, req);
+      req->stage = REQ_WRITE_REPLY;
+   }
+
+   if (req->stage == REQ_WRITE_REPLY) {
+      /* Keep writing until end or error */
+      if (Control_write_fd(fd, req->reply, &req->reply_offset) <= 0)
+         req->stage = REQ_CLOSE;
+   }
+
+   if (req->stage == REQ_CLOSE) {
+      /* Remove any callback */
+      a_IOwatch_remove_fd(fd, DIO_READ | DIO_WRITE);
+      dClose(fd);
+      Control_req_free(req);
+   }
+}
+
+/* Process incoming connection to control socket */
+static void Control_listen_cb(int fd, void *data)
+{
+   _MSG("Control_listen_cb called\n");
 
    int new_fd = accept(control_fd, NULL, NULL);
    if (new_fd < 0) {
@@ -120,158 +382,31 @@ static void Control_read_cb(int fd, void *data)
     * explicitly set all required flags on the socket returned from
     * accept().
     *
-    * So, disable O_NONBLOCK flag explicitly.
+    * So, enable O_NONBLOCK flag explicitly.
     */
-   if (fcntl(new_fd, F_SETFL, 0) == -1) {
+   if (fcntl(new_fd, F_SETFL, O_NONBLOCK) == -1) {
       MSG_ERR("control socket fcntl failed: %s\n", strerror(errno));
       exit(1);
    }
 
-   ssize_t r;
-   do {
-      /* Read characters one by one */
-      char buf[1];
-      r = read(new_fd, buf, 1);
-      _MSG("read = %zd\n", r);
-      if (r < 0) {
-         if (errno == EINTR) {
-            continue;
-         } else {
-            MSG_ERR("Control_read_cb %s\n", dStrerror(errno));
-            exit(1);
-         }
-      } else if (r == 1) {
-         dStr_append_l(dstr, buf, 1);
-      }
-      if (buf[0] == '\n')
-         break;
-   } while (r);
-
-   _MSG("got msg: %s\n", dstr->str);
-
-   char *cmd = dstr->str;
-   for (char *p = cmd; *p != '\0'; p++) {
-      if (*p == '\n' || *p == '\r') {
-         *p = '\0';
-         break;
-      }
-   }
-
-   FILE *f = fdopen(new_fd, "w");
-
-   if (f == NULL) {
-      MSG_ERR("fdopen failed: %s\n", dStrerror(errno));
-      exit(1);
-   }
-
-   BrowserWindow *bw = a_UIcmd_get_first_active_bw();
-   int do_close = 1;
-
-   if (strcmp(cmd, "help") == 0) {
-      fprintf(f, "0\n");
-      fprintf(f, "Commands return 0 on success or non-zero on error.\n");
-      fprintf(f, "Available commands:\n");
-      fprintf(f, " ping          Check if dillo replies correctly:\n");
-      fprintf(f, " pid           Print PID of selected dillo process\n");
-      fprintf(f, " reload        Reload the current tab\n");
-      fprintf(f, " ready         Exits with 0 if finished loading, 1 otherwise\n");
-      fprintf(f, " open URL      Open the given URL in the current tab\n");
-      fprintf(f, " url           Print the url in the current tab\n");
-      fprintf(f, " dump          Print the content of the current tab\n");
-      fprintf(f, " hdump         Print the HTTP headers of the current tab\n");
-      fprintf(f, " load          Replace the content in the current tab by stdin\n");
-      fprintf(f, " rawload       Replace the HTTP headers and content of the current\n");
-      fprintf(f, "               tab by stdin\n");
-      fprintf(f, " quit          Close dillo\n");
-      fprintf(f, " wait [T]      Wait until the current tab has finished loading\n");
-      fprintf(f, "               at most T seconds (default 60.0). Wait forever with\n");
-      fprintf(f, "               T set to 0.\n");
-   } else if (strcmp(cmd, "ping") == 0) {
-      fprintf(f, "0\npong\n");
-   } else if (strcmp(cmd, "pid") == 0) {
-      fprintf(f, "0\n%d\n", getpid());
-   } else if (strcmp(cmd, "reload") == 0) {
-      a_UIcmd_reload_all_active();
-      fprintf(f, "0\n");
-   } else if (strcmp(cmd, "ready") == 0) {
-      fprintf(f, "%d\n", a_UIcmd_has_finished(bw) ? 0 : 1);
-   } else if (strncmp(cmd, "open ", 5) == 0) {
-      a_UIcmd_open_urlstr(bw, cmd+5);
-      fprintf(f, "0\n");
-   } else if (strcmp(cmd, "url") == 0) {
-      fprintf(f, "0\n%s\n", a_UIcmd_get_location_text(bw));
-   } else if (strcmp(cmd, "dump") == 0) {
-      DilloUrl *url = a_Url_new(a_UIcmd_get_location_text(bw), NULL);
-      char *buf;
-      int size;
-      fprintf(f, "0\n");
-      if (a_Capi_get_buf(url, &buf, &size)) {
-         fwrite(buf, 1, size, f);
-      }
-      a_Url_free(url);
-   } else if (strcmp(cmd, "hdump") == 0) {
-      DilloUrl *url = a_Url_new(a_UIcmd_get_location_text(bw), NULL);
-      Dstr *header = a_Cache_get_header(url);
-      if (header == NULL) {
-         fprintf(f, "1\nCurrent url not cached");
-      } else {
-         fprintf(f, "0\n");
-         fwrite(header->str, 1, header->len, f);
-      }
-      a_Url_free(url);
-   } else if (strcmp(cmd, "load") == 0) {
-      cmd_load(f, new_fd, 0);
-   } else if (strcmp(cmd, "rawload") == 0) {
-      cmd_load(f, new_fd, 1);
-   } else if (strcmp(cmd, "quit") == 0) {
-      a_UIcmd_close_all_bw((void *)1);
-      fprintf(f, "0\n");
-   } else if (strncmp(cmd, "cmd ", 4) == 0) {
-      const char *cmdname = cmd + 4;
-      int ret = a_UIcmd_by_name(bw, cmdname);
-      fprintf(f, "%d\n", ret == 0 ? 0 : 1);
-   } else if (strcmp(cmd, "wait") == 0 || strncmp(cmd, "wait ", 5) == 0) {
-      float timeout = 60.0f; /* 1 minute by default */
-      /* Contains timeout argument? */
-      if (cmd[4] == ' ')
-         timeout = atof(&cmd[5]);
-      if (a_UIcmd_has_finished(bw)) {
-         fprintf(f, "0\n");
-      } else {
-         if (bw_waiting) {
-            fprintf(f, "1\nalready waiting\n");
-         } else {
-            bw_waiting = bw;
-            bw_waiting_file = f;
-            do_close = 0;
-
-            /* If timeout is 0 or negative, wait forever */
-            if (timeout > 0.0f)
-               a_Timeout_add(timeout, handle_wait_timeout, NULL);
-         }
-      }
-   } else {
-      fprintf(f, "1\nunknown command: %s\n", cmd);
-   }
-
-
-   dStr_free(dstr, 1);
-   if (do_close)
-      fclose(f);
-   // close(new_fd); // No need as fclose already does
+   struct control_req *req = Control_req_new(new_fd);
+   a_IOwatch_add_fd(new_fd, DIO_READ, Control_req_read_cb, req);
 }
 
 /* Timeout has passed, return error */
 static void handle_wait_timeout(void *data)
 {
-   assert(bw_waiting_file);
-   FILE *f = bw_waiting_file;
+   assert(bw_waiting);
+   assert(bw_waiting_req);
 
-   fprintf(f, "1\ntimeout\n");
-   fclose(f);
+   struct control_req *req = bw_waiting_req;
+   dStr_sprintfa(req->reply, "1\nCurrent url not cached");
+   req->stage = REQ_SWITCH_WRITE;
 
    bw_waiting = NULL;
-   bw_waiting_file = NULL;
+
+   /* Run another pass to do progress */
+   Control_req_read_cb(req->fd, req);
 }
 
 int a_Control_is_waiting(void)
@@ -284,18 +419,20 @@ void a_Control_notify_finish(BrowserWindow *bw)
    if (bw_waiting == NULL || bw_waiting != bw)
       return;
 
-   MSG("a_Control_notify_finish matched\n");
+   _MSG("a_Control_notify_finish matched\n");
 
-   assert(bw_waiting_file);
+   assert(bw_waiting_req);
 
-   FILE *f = bw_waiting_file;
-
-   fprintf(f, "0\n");
-   fclose(f);
+   struct control_req *req = bw_waiting_req;
+   dStr_sprintfa(req->reply, "0\n");
+   req->stage = REQ_SWITCH_WRITE;
 
    a_Timeout_actually_remove(handle_wait_timeout, NULL);
    bw_waiting = NULL;
-   bw_waiting_file = NULL;
+   bw_waiting_req = NULL;
+
+   /* Run another pass to do progress */
+   Control_req_read_cb(req->fd, req);
 }
 
 int a_Control_init(void)
@@ -362,7 +499,7 @@ int a_Control_init(void)
    control_fd = fd;
    control_path = dStrdup(addr.sun_path);
 
-   a_IOwatch_add_fd(control_fd, DIO_READ, Control_read_cb, NULL);
+   a_IOwatch_add_fd(control_fd, DIO_READ, Control_listen_cb, NULL);
 
    return 0;
 }
